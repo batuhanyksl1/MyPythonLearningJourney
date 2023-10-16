38c08b6ffea4d85ae975d3bd8ab1213d6e9289c2
Batuhan Yüksel <batuhanyksl1@gmail.com>
2023-10-16 14:04:17 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+#trial of writing a class and a method to return it.
+class adressbook:
+    def __init__(self,name,surname,phone_number,contact_id):
+        self.name=name
+        self.surname=surname
+        self.phone_number=phone_number
+        self.contact_id= 0
+    def get_contacts(self,name,surname):
+        return self.name,self.surname
+
+
+#nasıl getireceğimi bilmiyorum.
+user1=adressbook("Baturalp","Yüksel",5345241548,2)
