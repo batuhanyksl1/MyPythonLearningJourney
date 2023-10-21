04bdbaf13bf15ef0adab090ebdd52bc3359b2d5b
Batuhan Yüksel <146569785+batuhanyksl1@users.noreply.github.com>
2023-10-21 16:43:21 +0300

    create numpy1

---
+++ numpy1.py
@@ -0,0 +1,201 @@
+
+# importing
+import numpy as np
+
+# numpy basics
+array = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])  # 1*15 vector
+##
+
+array=np.array([2,3])
+batuhan=np.array([1,2,3,4,5,6,7,8,9])
+print(array.shape)
+
+a = array.reshape(3,5)
+print("shape: ",a.shape)
+print("dimension: ", a.ndim)
+
+print("data type: ",a.dtype.name)
+print("size: ",a.size)
+
+print("type: ",type(a))
+
+array1 = np.array([[1,2,3,4],[5,6,7,8],[9,8,7,5]],[[1,2,3,4],[5,6,7,8],[9,8,7,5],[[1,2,3,4],[5,6,7,8],[9,8,7,5]]])
+
+zeros = np.zeros((3,4))
+
+zeros[0,0] = 5
+print(zeros)
+
+np.ones((3,4))
+
+np.empty((2,3))
+
+a = np.arange(10,50,5)
+print(a)
+
+a = np.linspace(10,50,20)
+print(a)
+
+numpy.linspace(1, 100, num=50, endpoint=True, retstep=False, dtype=None, axis=0)
+
+
+# %% numpy basic operations
+
+a = np.array([1-,2,3])
+b = np.array([4,5,6])
+
+print(a+b)
+print(a-b)
+print(a**2)
+
+print(np.sin(a))
+
+print(a<2)
+
+
+a = np.array([[1,2,3],[4,5,6]])
+b = np.array([[1,2,3],[4,5,6]])
+
+# element wise prodcut
+print(a*b)
+
+# matrix prodcut
+a.dot(b.T)
+
+print(np.exp(a))
+
+a = np.random.random((5,5))
+
+print(a.sum())
+print(a.max())
+print(a.min())
+
+
+print(a.sum(axis=0))
+print(a.sum(axis=1))
+
+print(np.sqrt(a))
+print(np.square(a)) # a**2
+
+
+print(np.add(a,a))
+
+
+# %% indexing and slicing
+import numpy as np
+array = np.array([1,2,3,4,5,6,7])   #  vector dimension = 1
+
+print(array[0])
+
+print(array[0:4])
+
+reverse_array = array[::-1]
+print(reverse_array)
+
+
+array1 = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+
+print(array1[1,1])
+
+print(array1[:,1])
+
+
+print(array1[1,1:4])
+
+
+print(array1[-1,:])
+print(array1[:,-1])
+
+# %%
+# shape manipulation
+array = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+# flatten
+a = array.ravel()
+
+array2 = a.reshape(3,3)
+
+arrayT = array2.T
+
+print(arrayT.shape)
+
+
+array5 = np.array([[1,2],[3,4],[4,5]])
+
+
+#array5 = np.column_stack((array1,array1))
+
+
+# %% stacking arrays
+
+array1 = np.array([[1,2],[3,4]])
+array2 = np.array([[-1,-2],[-3,-4]])
+
+# veritical
+#array([[1, 2],
+#       [3, 4]])
+#array([[-1, -2],
+#       [-3, -4]])
+array3 = np.vstack((array1,array2))
+
+# horizontal
+#array([[1, 2],[-1, -2],
+#       [3, 4]],[-3, -4]]
+
+array4 = np.hstack((array1,array2))
+
+#%% convert and copy
+
+liste = [1,2,3,4]   # list
+
+array = np.array(liste) #np.array
+
+liste2 = list(array)
+
+a = np.array([1,2,3])
+
+b = a
+b[0] = 5
+c = a
+
+d =  np.array([1,2,3])
+
+e = d.copy()
+
+f = d.copy()
+
+"""numpy basics
+
+--array ile aralık belirleniyor ve burada ([]) kullanman gerekiyor. eger 
+matris oluşturacakasan o zaman ([[],[]]) şeklinde yazmalısın.
+
+a = np.arange(10,50,5) - 10 dan 50 ye kadar sayı yazdırır beşer beşer atlayarak
+
+
+a = np.linspace(10,50,20) - 10 ile 50 arasına 20 tane sayı seçer(eşit
+miktarda arttırarak.)
+
+
+"""
+
+ff = np.array([[[1,1,1]],[1,1,1],[1,1,1]])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+hgjhgjhg
+kuujjhkuyvoıcu
+
+
+
+
